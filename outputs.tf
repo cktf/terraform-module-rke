4ff25f7b910d1179e311f5f1447edfbe781eef56
KoLiBer <koliberr136a1@gmail.com>
2022-02-14 08:40:02 -0800

    fix: add description to output variables

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "ca_crt" {
   value     = data.k8sbootstrap_auth.this.ca_crt
   sensitive = true
+  description = "RKE CA Certificate"
 }
 
 output "kubeconfig" {
   value     = data.k8sbootstrap_auth.this.kubeconfig
   sensitive = true
+  description = "RKE Kubernetes Configuration"
 }
