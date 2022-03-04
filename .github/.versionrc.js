517af9f284194d62e1b06ec577f53cd5f0a19c87
KoLiBer <koliberr136a1@gmail.com>
2022-03-04 03:58:09 -0800

    feat: migrate from semantic-release to standard-version

---
+++ .github/.versionrc.js
@@ -0,0 +1,41 @@
+const branchChannels = {
+    main: undefined,
+    "([0-9])?(.{+([0-9]),x}).x": undefined,
+    next: "next",
+};
+
+const currentBranch = require("child_process")
+    .execSync("git branch --show-current")
+    .toString();
+const currentChannel =
+    branchChannels[
+        Object.keys(branchChannels).find((value) =>
+            new RegExp(value).test(currentBranch)
+        )
+    ];
+
+require("fs").writeFileSync("VERSION", "");
+require("fs").writeFileSync("CHANNEL", currentChannel || "latest");
+require("fs").copyFileSync(".env.example", ".env");
+
+module.exports = {
+    tagPrefix: "",
+    packageFiles: [],
+    infile: "CHANGELOG",
+    prerelease: currentChannel,
+    bumpFiles: [
+        {
+            filename: "VERSION",
+            updater: {
+                readVersion: () => null,
+                writeVersion: (_, version) => {
+                    return version;
+                },
+            },
+        },
+    ],
+    skip: {
+        commit: true,
+        tag: true,
+    },
+};
