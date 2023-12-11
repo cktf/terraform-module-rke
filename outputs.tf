120292b793c4201cf9128568d709759a698a28ad
KoLiBer <koliberr136a1@gmail.com>
2023-12-12 00:30:18 +0330

    fix: base64decode certs and key

--- outputs.tf
+++ outputs.tf
@@ -5,19 +5,19 @@ output "host" {
 }
 
 output "client_key" {
-  value       = yamldecode(ssh_sensitive_resource.kubeconfig.result).clusters[0].cluster.certificate-authority-data
+  value       = base64decode(yamldecode(ssh_sensitive_resource.kubeconfig.result).clusters[0].cluster.certificate-authority-data)
   sensitive   = true
   description = "Cluster Client Key"
 }
 
 output "client_cert" {
-  value       = yamldecode(ssh_sensitive_resource.kubeconfig.result).users[0].user.client-certificate-data
+  value       = base64decode(yamldecode(ssh_sensitive_resource.kubeconfig.result).users[0].user.client-certificate-data)
   sensitive   = true
   description = "Cluster Client Certificate"
 }
 
 output "ca_cert" {
-  value       = yamldecode(ssh_sensitive_resource.kubeconfig.result).users[0].user.client-key-data
+  value       = base64decode(yamldecode(ssh_sensitive_resource.kubeconfig.result).users[0].user.client-key-data)
   sensitive   = true
   description = "Cluster CA Certificate"
 }
